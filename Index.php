d259572501c58cb74d616f1ff556991eeaeac0a8
DRJUNKERMAN <omicronblasterx@hotmail.com>
2014-09-20 21:01:25 -0500

    Creando el menu principal xD
    Estructura del Menu principal para crear y llamar a las clases controladoras

---
+++ Index.php
@@ -0,0 +1,20 @@
+<?php
+
+    $opcion = $_GET['opcion'];
+    
+    if( $opcion == 'Vehículo' )
+    {   
+        
+    }
+    else
+        if( $opcion == 'usuario' )
+        {
+        
+        }
+        
+  if( $opcion != null )
+      
+        
+
+
+?>
