5a55e8444b7b726718d19dff13bd5e4e6a060d8b
DRJUNKERMAN <omicronblasterx@hotmail.com>
2014-09-21 12:21:40 -0500

    se agrego el contenido al modelo usuario

--- MODELOS/ModeloUsuario.php
+++ MODELOS/ModeloUsuario.php
@@ -2,9 +2,14 @@
 
     class modeloUsuario{
         //private datos
-        
-        public function crear(/*datos*/){
+            private $nombre, $direccion, $codigo, $correo;
             
+            
+        public function crear($nombre,$direccion,$codigo,$correo){
+            $this->nombre = $nombre;
+            $this->direccion = $direccion;
+            $this->codigo = $codigo;
+            $this->correo = $correo;
         }  
         
         function __construct(){
