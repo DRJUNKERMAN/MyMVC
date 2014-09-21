6da0114d28544fd7372fcc1ba62e9a041fd7b2ff
DRJUNKERMAN <omicronblasterx@hotmail.com>
2014-09-21 12:16:36 -0500

    creacion del contenido del controlador vehiculo ;-)

--- CONTROLADORES/ControladorVehiculo.php
+++ CONTROLADORES/ControladorVehiculo.php
@@ -2,24 +2,35 @@
 
      class vehiculo{
      
-         private $modelo;
+         private $modeloVehiculo;
          
-         public function ejecutar( )
-         {
-         
-         
+         public function ejecutar( ){
+             
+             if( $_GET['act'] == 'crear' )
+                 $this->crear();
+                 
+             
          }
          
          private function crear( )
          {
-         
-         
+             $marca = $_POST['marca'];
+             $modelo = $_POST['modelo'];
+             $año = $_POST['año'];
+             $precio = $_POST['precio'];
+             
+             $resultado = $this->modelo->crear($marca,$modelo,$año,$precio);
+             
+             if( $resultado )
+                 require_once 'VISTAS/vistaTemporal.php';
+             else
+                 require_once 'VISTAS/vistaTemporal.php';
          }
          
          function __construct()
          {
-             
-         
+             require_once 'MODELOS/ModeloVehiculo.php';
+             $this->modeloVehiculo = new modeloVehiculo();
          }
          
          
