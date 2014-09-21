7d868c529c8173296922b9eee00ad8c3c103872e
DRJUNKERMAN <omicronblasterx@hotmail.com>
2014-09-21 13:57:51 -0500

    contenido modelo vehiculo

--- MODELOS/ModeloVehiculo.php
+++ MODELOS/ModeloVehiculo.php
@@ -2,10 +2,13 @@
 
     class ModeloVehiculo{
       //private datos
-    
-       public function create(){
-       
-       
+      private $marca, $modelo, $año, $precio;
+      
+       public function create($marca, $modelo, $año, $precio){
+           $this->marca = $marca;
+           $this->modelo = $modelo;
+           $this->año = $año;
+           $this->precio = $precio;
        }
        
        function __construct(){
