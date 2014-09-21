7766c9768a99cbd6b81748eea9923f8d12c13aa5
DRJUNKERMAN <omicronblasterx@hotmail.com>
2014-09-20 22:50:01 -0500

    Estructura de la clase controlador vehiculo
    Donde crearemos la instancia del modelo vehículo y le pasaremos como parámetro los datos a agregar en la Base de datos

---
+++ CONTROLADORES/Vehiculo.php
@@ -0,0 +1,33 @@
+<?php
+
+     class vehiculo{
+     
+         private $modelo;
+         
+         public function ejecutar( )
+         {
+         
+         
+         }
+         
+         private function crear( )
+         {
+         
+         
+         }
+         
+         function __construct()
+         {
+             
+         
+         }
+         
+         
+     
+     }
+    
+
+
+
+
+?>
