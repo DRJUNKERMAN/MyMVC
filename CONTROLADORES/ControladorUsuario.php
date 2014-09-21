47bcd68442a4703ea29503b7ab1bba386f5a0061
DRJUNKERMAN <omicronblasterx@hotmail.com>
2014-09-21 12:00:17 -0500

    contenido del modelo usuario

--- CONTROLADORES/ControladorUsuario.php
+++ CONTROLADORES/ControladorUsuario.php
@@ -3,20 +3,33 @@
           
          private $modeloUsuario;
          
-         
          public function ejecutar(){
-         
-         
-         
+             
+               if( $GET['act'] == 'crear' )
+                   $this->crear();
+               
          }
          
          private function crearModeloUsuario(){
+             //obtener datos de formulario html
+             $nombre = $POST['nombre'];
+             $direccion = $POST['direccion'];
+             $codigo = $POST['codigo'];
+             $correo = $POST['correo'];
              
+             $resultado = $this->modeloUsuario;
+             
+             if($resultado)
+                 require_once 'VISTAS/vistaTemporal.php';
+             else
+                require_once 'VISTAS/vistaTemporal.php'
          }
          
+         
+         
          function __construct(){
-         
-         
+              require_once 'MODELOS/ModeloUsuario.php';
+              $this->modeloUsuario = new modeloUsuario();
          }
     
     }
